43d226744034ba610cb126facb1b6677109035e9
Benjamim-EP <bannk18@gmail.com>
2024-08-29 08:58:20 -0300

    Versão Inicial

---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,40 @@
+import styles from './SkillsStyles.module.css';
+import checkMarkIconDark from '../../assets/checkmark-dark.svg';
+import checkMarkIconLight from '../../assets/checkmark-light.svg';
+import SkillList from '../../common/SkillList';
+import { useTheme } from '../../common/ThemeContext';
+
+function Skills() {
+  const { theme } = useTheme();
+  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+
+  return (
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="Azure" />
+        <SkillList src={checkMarkIcon} skill="Spring Framework" />
+        <SkillList src={checkMarkIcon} skill="Node" />
+      </div>
+      
+      <hr />
+      
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="HTML" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Power BI" />
+        <SkillList src={checkMarkIcon} skill="AWS" />
+        <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="RDBMS" />
+        <SkillList src={checkMarkIcon} skill="Tensorflow" />
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
